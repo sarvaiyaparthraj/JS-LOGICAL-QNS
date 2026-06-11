35eaa25dfabfcffca47496fce08b5682149d99b5
Parthrajsinh Sarvaiya <sarvaiyaparthrajsinh1818@gmail.com>
2026-06-11 13:22:26 +0530

    Add files via upload

---
+++ 1ST/1.code execution.js
@@ -0,0 +1,25 @@
+
+
+var n = 2;
+
+function square(n) {
+  const result = n * n;
+  return result;
+}
+
+var square2 = square(n);
+
+var square4 = square(4);
+
+console.log(square2, square4);
+
+
+
+// Manual Method:
+
+var n = 2;
+
+var square2 = 2 * 2;
+var square4 = 4 * 4;
+
+console.log(square2, square4);
